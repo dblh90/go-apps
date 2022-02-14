09b6b30bafc7c59fd68b5ba911f5d50b3c03f44f
doubleH90 <hamzahasan1990@gmail.com>
2022-02-14 18:52:59 +0200

    Add other assigning methods

--- main.go
+++ main.go
@@ -10,10 +10,60 @@ func main() {
 	assignPointer()
 	assignArray()
 	assignSlice()
+	assignSliceFromSlice()
+	assignMap()
+	assignStruct()
+}
+
+func assignStruct() {
+
+	type user struct {
+		Id        int
+		FirstName string
+		LastName  string
+	}
+
+	var u user
+	u.Id = 1
+	u.FirstName = "Hamza"
+	u.LastName = "Hasan"
+	fmt.Println(u)
+
+	u1 := user{Id: 2, FirstName: "Hamza", LastName: "Hasan"}
+	fmt.Println(u1)
+}
+
+func assignMap() {
+	m := map[string]int{"Hey": 1}
+	fmt.Println(m)
+	fmt.Println(m["Hey"])
+
+	m["hello"] = 2
+	fmt.Println(m)
+
+	delete(m, "Hey")
+	fmt.Println(m)
+}
+
+func assignSliceFromSlice() {
+	slc := []int{1, 2, 3, 4, 5, 6}
+
+	slc1 := slc[2:]  // Specify starting index, no end index
+	slc2 := slc[:1]  // Specify end index, no start index
+	slc3 := slc[3:4] // Specify start & end index.
+
+	fmt.Println(slc, slc1, slc2, slc3)
 }
 
 func assignSlice() {
-	panic("unimplemented")
+	arr := [10]int{1, 2, 3, 4, 5, 6}
+	slc := arr[:]
+	fmt.Println(slc)
+
+	fmt.Println("Appending to Slice")
+	slc = append(slc, 49)
+
+	fmt.Println(slc)
 }
 
 func assignArray() {
